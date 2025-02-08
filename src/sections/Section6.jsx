b0958b9bdf9b1b9a5f17d199bd8d58ae10281e3c
swostikpati <swostikpati@gmail.com>
2025-02-09 03:25:26 +0400

    till section 7

--- src/sections/Section6.jsx
+++ src/sections/Section6.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { motion } from "framer-motion";
 import "./Section6.css";
-
+import FadeInWrapper from "@/components/FadeInWrapper";
+import section6 from "@/assets/images/section6.png";
 export default function Section6() {
   return (
     <div className="section6">
@@ -15,9 +16,22 @@ export default function Section6() {
       <motion.div
         initial={{ scale: 1.2 }} // Start at 100%
         animate={{ scale: 1.5 }} // Animate to 120%
-        transition={{ duration: 7, ease: "easeInOut" }} // 5 seconds, smooth transition
+        transition={{ duration: 8, ease: "easeInOut" }} // 5 seconds, smooth transition
         className="battery-hero"
       ></motion.div>
+      <FadeInWrapper className="desc">
+        <p>
+          The new MacBook Pro has the{" "}
+          <span className="highlight">longest battery life ever in a Mac</span>{" "}
+          — up to 24 hours — and supports fast charge, allowing it to charge up
+          to 50 percent in just 30 minutes. 50 All models provide the same
+          performance whether they’re plugged in or not, so you can spend more
+          time thinking about an outlet for your passion, not your laptop.
+        </p>
+      </FadeInWrapper>
+      <FadeInWrapper className="desc-img-wrapper">
+        <img src={section6} alt="metrics" className="desc-img" />
+      </FadeInWrapper>
     </div>
   );
 }
---
+++ src/sections/Section7.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
+
+export default function Section7() {
+  // Track the scroll progress
+  const { scrollYProgress } = useScroll();
+
+  // Map scroll progress (0 → 1) to scale values (1.5 → 0.7)
+  const scale = useTransform(scrollYProgress, [0, 1], [6, 0.7]);
+  return (
+    <div className="section7">
+      <div className="text">
+        <p className="heading">DISPLAY</p>
+        <p className="subheading">See it all in a new light.</p>
+      </div>
+      <motion.div
+        style={{
+          width: "100vw",
+          height: "135vh",
+          backgroundImage: `url("https://www.apple.com/v/macbook-pro/ao/images/overview/themes/display/display_startframe__s3kff4jwzhuq_xlarge.jpg")`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          marginTop: "40vh",
+          scale, // Apply the dynamic scaling
+        }}
+      ></motion.div>
+    </div>
+  );
+}
